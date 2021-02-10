b15f6afdbf52514cb1a1a2151278471c0030b224
rita-mordio <ritamordio0716@gmail.com>
2021-02-10 17:31:49 +0900

    Albums 페이지 영역 구분

--- src/pages/Albums.js
+++ src/pages/Albums.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Container, Divider, Grid} from "semantic-ui-react";
 
 const Album = () => {
   return(
-      <div>album list</div>
+      <Container>
+        <Grid>
+          <Grid.Column computer={4} tablet={6} mobile={16}>
+            <div>영역1</div>
+          </Grid.Column>
+          <Grid.Column computer={12} tablet={10} mobile={16}>
+            <div>영역2</div>
+          </Grid.Column>
+        </Grid>
+        <Divider />
+        <div>영역3</div>
+      </Container>
   )
 }
 
