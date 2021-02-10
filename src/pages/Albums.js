9fdcb6c3213d205e6a8c2f8f4d408da5023f9156
rita-mordio <ritamordio0716@gmail.com>
2021-02-10 17:51:22 +0900

    정렬 Dropdown 컴포넌트 추가

--- src/pages/Albums.js
+++ src/pages/Albums.js
@@ -1,21 +1,31 @@
-import React from 'react'
-import {Container, Divider, Grid} from "semantic-ui-react";
+import React, { useState } from "react";
+import { Container, Divider, Grid } from "semantic-ui-react";
+
+import OrderDropdown from "../components/OrderDropdown";
 
 const Album = () => {
-  return(
-      <Container>
-        <Grid>
-          <Grid.Column computer={4} tablet={6} mobile={16}>
-            <div>영역1</div>
-          </Grid.Column>
-          <Grid.Column computer={12} tablet={10} mobile={16}>
-            <div>영역2</div>
-          </Grid.Column>
-        </Grid>
-        <Divider />
-        <div>영역3</div>
-      </Container>
-  )
-}
+  const [searchCriteria, setSearchCriteria] = useState({
+    searchText: "",
+    orderType: "",
+  });
 
-export default Album
+  return (
+    <Container className="albums-container">
+      <Grid>
+        <Grid.Column computer={4} tablet={6} mobile={16}>
+          <OrderDropdown
+            searchCriteria={searchCriteria}
+            setSearchCriteria={setSearchCriteria}
+          />
+        </Grid.Column>
+        <Grid.Column computer={12} tablet={10} mobile={16}>
+          <div>영역2</div>
+        </Grid.Column>
+      </Grid>
+      <Divider />
+      <div>영역3</div>
+    </Container>
+  );
+};
+
+export default Album;
