8b82f8c23a771a15447ef1bca470c84befa2a9fe
rita-mordio <ritamordio0716@gmail.com>
2021-02-10 17:20:08 +0900

    Album, Albums 페이지 추가 및 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
+import Albums from "./pages/Albums";
+import Album from "./pages/Album";
+
 const App = () => {
   return (
-    <>
-    </>
+      <>
+        <Route path="/" component={Albums} exact={true} />
+        <Route path="/album" component={Album} />
+      </>
   );
 };
 
