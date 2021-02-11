edad3ca76c1213efb0bfd3a01b331587e76a5e5a
rita-mordio <ritamordio0716@gmail.com>
2021-02-11 09:22:50 +0900

    페이지 이동시 화면 최상단으로 이동하는 ScrollToTop 컴포넌트 추가

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 
+import ScrollToTop from "./components/ScrollToTop";
+
 import "./common.css";
 
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById("root")
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -8,60 +8,67 @@ const Album = ({ history }) => {
 
   const onClick = () => {
     history.push("/");
-  }
+  };
 
   return (
     <Container className="album-container">
       <FadeIn>
-        <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell colSpan="3">Album Detail</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell textAlign="center" colSpan="3">
-                <Image
-                  className="detail-image"
-                  src={albumItem["im:image"][2].label}
-                  size="medium"
-                  rounded
-                />
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>제목</Table.Cell>
-              <Table.Cell>{albumItem["im:name"].label}</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>아티스트</Table.Cell>
-              <Table.Cell>{albumItem["im:artist"].label}</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>카테고리</Table.Cell>
-              <Table.Cell>{albumItem.category.attributes.label}</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>가격</Table.Cell>
-              <Table.Cell>{albumItem["im:price"].label}</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>발매일</Table.Cell>
-              <Table.Cell>
-                {albumItem["im:releaseDate"]["attributes"].label}
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>권리</Table.Cell>
-              <Table.Cell>{albumItem.rights.label}</Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
-        <Button className='back-button' floated='right' icon labelPosition='left' onClick={onClick}>
-          <Icon name='left arrow' />
-          이전 화면으로
-        </Button>
+        <div>
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell colSpan="3">앨범 상세정보</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell textAlign="center" colSpan="2">
+                  <Image
+                    className="detail-image"
+                    src={albumItem["im:image"][2].label}
+                    size="medium"
+                    rounded
+                  />
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>제목</Table.Cell>
+                <Table.Cell>{albumItem["im:name"].label}</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>아티스트</Table.Cell>
+                <Table.Cell>{albumItem["im:artist"].label}</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>카테고리</Table.Cell>
+                <Table.Cell>{albumItem.category.attributes.label}</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>가격</Table.Cell>
+                <Table.Cell>{albumItem["im:price"].label}</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>발매일</Table.Cell>
+                <Table.Cell>
+                  {albumItem["im:releaseDate"]["attributes"].label}
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>권리</Table.Cell>
+                <Table.Cell>{albumItem.rights.label}</Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+          <Button
+            floated="right"
+            icon
+            labelPosition="left"
+            onClick={onClick}
+          >
+            <Icon name="left arrow" />
+            이전 화면으로
+          </Button>
+        </div>
       </FadeIn>
     </Container>
   );
